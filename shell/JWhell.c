9758fa476d79ae489ba2f559e73b5141501d8978
William Marais <79162124+SAED2906@users.noreply.github.com>
2023-08-18 16:11:21 +0200

    Update JWhell.c

--- shell/JWhell.c
+++ shell/JWhell.c
@@ -12,55 +12,60 @@ static char *prompt;
 
 /* Prototyping Functions */
 static void startup(void);
-static void refresh_prompt(char* lineptr, size_t n);
+static void refresh_prompt(char* lineptr, size_t n, char* tokens);
 /* Main */ 
 int main(int ac, char **argv){
-	char *lineptr;
-	size_t n = 0; 
-	prompt = "(JW) $ ";
-	strcpy(vn, "1.0.0");
-  	startup();
+  char *lineptr;
+  size_t n = 0; 
+  char *tokens[3][1024];
 
-  	/* declaring void variables */
-  	(void)ac; (void)argv;
-  	while (1 == 1){
-  		refresh_prompt(lineptr, n);
-  	}
+  prompt = "(JW) $ ";
+  strcpy(vn, "1.0.0");
+  startup();
 
-  	free(lineptr);
-  	return (0);
+  /* declaring void variables */
+  (void)ac; (void)argv;
+  while (1 == 1){
+  	refresh_prompt(lineptr, n, tokens);
+  }
+
+  free(lineptr);
+  return (0);
 }
 
 void startup(void)
 {
-  	printf("-----------------------------------------\n");
-  	printf("----| Discalaimer:                  |----\n");
-  	printf("----| This is a\033[0;34m WIP  \033[0m               |----\n");
-  	printf("----|     Made by William & John    |----\n");
-  	printf("----| • There may be critical bugs  |----\n");
-  	printf("----| • It lacks many features      |----\n");
-  	printf("-----------------------------------------\n");
-  	printf("----> Version: %s\n", vn); 
-  	printf("-----------------------------------------\n");
+  printf("-----------------------------------------\n");
+  printf("----| Discalaimer:                  |----\n");
+  printf("----| This is a\033[0;34m WIP  \033[0m               |----\n");
+  printf("----|     Made by William & John    |----\n");
+  printf("----| • There may be critical bugs  |----\n");
+  printf("----| • It lacks many features      |----\n");
+  printf("-----------------------------------------\n");
+  printf("----> Version: %s\n", vn); 
+  printf("-----------------------------------------\n");
 }
 /* TODO: 
 *  we need to add a parser 
 */
-void get_tokens(char line) {
-    /* 
-    separate lines into tokens
-    replace whitespaces (strtok)
-    parse commands 
-    code gen 
-    */
+void get_tokens(char* tokens, char* lineptr) {
+	char *token;
+	int i = 0;
+
+	token = strtok(*lineptr, 32);
+	while( token != NULL ) {
+      token = strtok(NULL, 32);
+	  strcpy(tokens[i++], token);
+   }
 }
 /* 
 * TODO: refresh current prompt to the newest generation
 */
-void refresh_prompt(char* lineptr, size_t n)
+void refresh_prompt(char* lineptr, size_t n, char* tokens)
 {
 	printf("%s | %s ", getcwd(cwd, sizeof(cwd)), prompt);
-    	getline(&lineptr, &n, stdin);
-  	get_tokens(lineptr);
-	printf("%s", lineptr);
+    getline(&lineptr, &n, stdin);
+    get_tokens(tokens, lineptr);
+	printf("%s", tokens[0]);
+	/* chdir(".."); */
 }
