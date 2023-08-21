cb485969073b68f488dbe47c55d0ab1254a052c5
SAED-Pi <25874845@sun.ac.za>
2023-08-21 10:50:02 +0200

    updated tokenisation

--- shell/JWhell.c
+++ shell/JWhell.c
@@ -35,15 +35,15 @@ int main(int ac, char **argv){
 
 void startup(void)
 {
-  printf("-----------------------------------------\n");
-  printf("----| Discalaimer:                  |----\n");
-  printf("----| This is a\033[0;34m WIP  \033[0m               |----\n");
-  printf("----|     Made by William & John    |----\n");
-  printf("----| • There may be critical bugs  |----\n");
-  printf("----| • It lacks many features      |----\n");
-  printf("-----------------------------------------\n");
-  printf("----> Version: %s\n", vn); 
-  printf("-----------------------------------------\n");
+  	printf("-----------------------------------------\n");
+  	printf("----| Discalaimer:                  |----\n");
+  	printf("----| This is a\033[0;34m WIP  \033[0m               |----\n");
+  	printf("----|     Made by William & John    |----\n");
+  	printf("----| • There may be critical bugs  |----\n");
+  	printf("----| • It lacks many features      |----\n");
+ 	printf("-----------------------------------------\n");
+  	printf("----> Version: %s\n", vn); 
+  	printf("-----------------------------------------\n");
 }
 /* TODO: 
 *  we need to add a parser 
@@ -55,16 +55,23 @@ void get_tokens(char* tokens, char* lineptr) {
 	 * seg fault, we should store tokens using a delimeter of some sort bcus rn
 	 * istn it just adding to the string? (without anyway of seperating the
 	 * tokens what if a token is longer than 1 char? am i being sill?
-	 * i dont want to code so i just added comments that i think we need lol*/
+	 * i dont want to code so i just added comments that i think we need lol
+	 */
 	
 	char *token;
+	char *saveptr;
+	
 	int i = 0;
 
-	token = strtok(*lineptr, 32);
-	while( token != NULL ) {
-      	token = strtok(NULL, 32);
-	strcpy(tokens[i++], *token);
-   }
+	token = strtok_r(lineptr, " ", &saveptr);
+	strcpy(tokens[i], token);
+	while (token != NULL) {
+		i++;
+		token = strtok_r(NULL, " ", &saveptr);
+		strcpy(tokens[i], token);
+	}
+	
+
 }
 /* 
 * TODO: refresh current prompt to the newest generation
@@ -72,8 +79,8 @@ void get_tokens(char* tokens, char* lineptr) {
 void refresh_prompt(char* lineptr, size_t n, char* tokens)
 {
 	printf("%s | %s ", getcwd(cwd, sizeof(cwd)), prompt);
-    	getline(&lineptr, &n, stdin);
-    	get_tokens(tokens, lineptr);
+    getline(&lineptr, &n, stdin);
+    get_tokens(tokens, lineptr);
 	printf("%s", tokens[0]);
 	/* chdir(".."); */
 }
